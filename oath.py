03b773202b955f2a73de233e48733c7a92be0af0
brendes6 <brendes6@gmail.com>
2025-05-29 23:16:16 -0400

    Update oath settings

--- oath.py
+++ oath.py
@@ -9,7 +9,8 @@ def create_playlist(df, playlist_name):
     try:
         sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE, client_id=st.secrets["client_id"],
                                                     client_secret=st.secrets["client_secret"],
-                                                    redirect_uri=st.secrets["redirect_uri"]))
+                                                    redirect_uri=st.secrets["redirect_uri"],
+                                                    open_browser=False))
 
         song_ids = df['track_id'][:30].tolist()
 
